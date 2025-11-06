4c5398124277c48e2c2aee656ffb3410c377c66f
Ahmed Rashed <ahmed.m.rashed@gmail.com>
2025-11-06 13:22:42 +0200

    Completed

--- ISA_script.m
+++ ISA_script.m
@@ -12,35 +12,30 @@ T_0_row=[288.16,216.66,216.66,282.66,282.66,165.66,165.66];
 p_0_row=[101330,22633,2488.7,120.45,58.323,1.0095,0.10444];
 a_0_row=[-.0065,.003,-.0045,.004];
 
+N_layers=length(h_G0_row)-1;
 N_layer=100;
-h_G_rows=linspace(h_G0_row(1),h_G0_row(2),N_layer);
-h_G1_row=linspace(h_G0_row(1),h_G0_row(2),N_layer);
-h_G2_row=linspace(h_G0_row(2),h_G0_row(3),N_layer);
-h_G3_row=linspace(h_G0_row(3),h_G0_row(4),N_layer);
-h_G4_row=linspace(h_G0_row(4),h_G0_row(5),N_layer);
-h_G5_row=linspace(h_G0_row(5),h_G0_row(6),N_layer);
-h_G6_row=linspace(h_G0_row(6),h_G0_row(7),N_layer);
-h_G7_row=linspace(h_G0_row(7),h_G0_row(8),N_layer);
-h_G_row=[h_G1_row,h_G2_row,h_G3_row,h_G4_row,h_G5_row,h_G6_row,h_G7_row];
+
+h_G_rows=nan(N_layers,N_layer);
+for n=1:N_layers
+    h_G_rows(n,:)=linspace(h_G0_row(n),h_G0_row(n+1),N_layer);
+end
+
+T_rows=nan(N_layers,N_layer);
+p_rows=nan(N_layers,N_layer);
+
+n_grad_layers_vec=1:2:7;
+T_rows(n_grad_layers_vec,:)=T_0_row(n_grad_layers_vec).'+a_0_row.'.*(h_G_rows(n_grad_layers_vec,:)-h_G0_row(n_grad_layers_vec).');
+p_rows(n_grad_layers_vec,:)=p_0_row(n_grad_layers_vec).'.*(T_rows(n_grad_layers_vec,:)./T_0_row(n_grad_layers_vec).').^(-g_0./a_0_row.'./R);
+
+n_iso_layers_vec=2:2:7;
+T_rows(n_iso_layers_vec,:)=repmat(T_0_row(n_iso_layers_vec).',1,N_layer);
+p_rows(n_iso_layers_vec,:)=p_0_row(n_iso_layers_vec).'.*exp(-g_0.*(h_G_rows(n_iso_layers_vec,:)-h_G0_row(n_iso_layers_vec).')./R./T_0_row(n_iso_layers_vec).');
+
+h_G_row=reshape(h_G_rows.',1,[]);
+T_row=reshape(T_rows.',1,[]);
+p_row=reshape(p_rows.',1,[]);
 
 h_row=r.*h_G_row./(r+h_G_row);
-
-T_grad_rows=T_0_row(1:2:7).'+a_0_row(1:4).'.*(h_G1_row-h_G0_row(1));
-T_iso_rows=repmat(T_0_row(2:2:6).',1,N_layer);
-T_row=[T_grad_rows(1,:),T_iso_rows(1,:),T_grad_rows(2,:),T_iso_rows(2,:),T_grad_rows(3,:),T_iso_rows(3,:),T_grad_rows(4,:)];
-
-p_grad_rows=p_0_row(1:2:7).'.*(T_grad_rows./T_0_row(1:2:7)).^(-g_0./a_0_row(1:4)./R);
-p_iso_row=p_0_row(2:2:6).'.*exp(-g_0.*(h_G2_row-h_G0_row(2))./R./T_0_row(2));
-
-p1_row=p_0_row(1).*(T1_row./T_0_row(1)).^(-g_0./a_0_row(1)./R);
-p2_row=p_0_row(2).*exp(-g_0.*(h_G2_row-h_G0_row(2))./R./T_0_row(2));
-p3_row=p_0_row(3).*(T3_row./T_0_row(3)).^(-g_0./a_0_row(2)./R);
-p4_row=p_0_row(4).*exp(-g_0.*(h_G4_row-h_G0_row(4))./R./T_0_row(4));
-p5_row=p_0_row(5).*(T5_row./T_0_row(5)).^(-g_0./a_0_row(3)./R);
-p6_row=p_0_row(6).*exp(-g_0.*(h_G6_row-h_G0_row(6))./R./T_0_row(6));
-p7_row=p_0_row(7).*(T7_row./T_0_row(7)).^(-g_0./a_0_row(4)./R);
-p_row=[p1_row,p2_row,p3_row,p4_row,p5_row,p6_row,p7_row];
-
 rho_row=p_row./R./T_row;
 a_row=sqrt(gamma.*R.*T_row);
 
